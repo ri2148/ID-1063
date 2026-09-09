9e205aa93fd1d0561acd27f89542a464b73f8320
Arjun Iyengar <ee26btech11202@iith.ac.in>
2026-09-09 15:25:35 +0530

    third q modify

--- 09-09-2026/lab6_3.c
+++ 09-09-2026/lab6_3.c
@@ -3,6 +3,15 @@
 
 #include<stdio.h>
 #include<string.h>
+
+//swap function
+void swap(char *a, char *b){
+char temp='\0';
+temp=*a;
+*a=*b;
+*b=temp;}
+
+//Main program begins
 int main(void){
 char string[100]; //Declaring the string
 printf("Enter any word: ");
@@ -11,10 +20,7 @@ fgets(string, sizeof(string), stdin); //Taking the input
 string[strcspn(string, "\n")] = '\0'; // To clear \n added by fgets 
 
 //Code for swapping
-char temp='\0';
-temp=string[0];
-string[0]=string[1];
-string[1]=temp;
+swap(&string[0], &string[1]);
 
 //Printing modified string
 printf("Modified word: %s ", string);
