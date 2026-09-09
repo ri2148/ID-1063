076236f25a87daed3b4b834f97ddff2440a446e0
Arjun Iyengar <ee26btech11202@iith.ac.in>
2026-09-09 15:16:04 +0530

    third q

--- 09-09-2026/lab6_3.c
+++ 09-09-2026/lab6_3.c
@@ -1,14 +1,21 @@
+// Code by Arjun
+// Date: 09-09-2026
+
 #include<stdio.h>
+#include<string.h>
 int main(void){
-char string[100];
+char string[100]; //Declaring the string
 printf("Enter any word: ");
 
-fgets(string, sizeof(string), stdin);
-string[strcspn(string, "\n")] = '\0';
+fgets(string, sizeof(string), stdin); //Taking the input
+string[strcspn(string, "\n")] = '\0'; // To clear \n added by fgets 
 
+//Code for swapping
 char temp='\0';
 temp=string[0];
 string[0]=string[1];
 string[1]=temp;
+
+//Printing modified string
 printf("Modified word: %s ", string);
 }
