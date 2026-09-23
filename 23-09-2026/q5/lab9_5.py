0b940278644ca08f36d38912c6143316176e7e78
Arjun Iyengar <ee26btech11202@iith.ac.in>
2026-09-23 16:55:05 +0530

    commit

--- 23-09-2026/q5/lab9_5.py
+++ 23-09-2026/q5/lab9_5.py
@@ -3,19 +3,17 @@
 import numpy as np
 
 def solve():
-    print("Input n:")
-    n = int(input("Input: "))
+    n = int(input("Input n: "))
 
     # Read n floating-point numbers into a NumPy array
     print("Enter into array:")
-    a = np.array([float(input()) for _ in range(n)], dtype=float)
-
+    a = np.fromstring(input(), sep=" ", count=n)
     # Read the tolerance value
     print("Tolerance:")
     tolerance = float(input())
 
     # Output the result of firststable
     print(f"Output: {firststable(a, n, tolerance)}")
-print("Output:")
+
 solve()
 
